5e0b2c852728e17ceb9a37995a5c49d0e3a094ef
Malathi92 <103888118+Malathi92@users.noreply.github.com>
2022-05-15 09:35:30 +0400

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+		
+		int number=5;
+		int i,fact=1;
+		for(i=1;i<=number;i++)
+		{	
+			fact=fact*i;
+			
+		}
+		System.out.println("factorial of "  + number+  " is:"+fact);
+		// TODO Auto-generated method stub
+
+	}
+
+}
