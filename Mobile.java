17a9172ab0ade00400ac73a1760c807366a4d6c0
Malathi92 <103888118+Malathi92@users.noreply.github.com>
2022-05-18 08:10:24 +0400

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Mobile {
+	public void makeCall()
+	{
+		System.out.println("can make call");
+	}
+
+	public void sendMsg()
+	{
+		System.out.println("can send msg");
+	
+	}
+	public static void main(String[] args) {
+		Mobile myMobile=new Mobile();
+		myMobile.makeCall();
+		myMobile.sendMsg();
+		System.out.println("my new mobile ");
+		// TODO Auto-generated method stub
+
+	}
+
+}
